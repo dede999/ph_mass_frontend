95d4ed87a95b1dd53e5f87611c669f31b5284822
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2022-05-29 18:29:38 -0300

    Include basic backend call
    This is useful to wake up the backend application, as it sleeps from time
    to time

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,18 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import { useFetch } from "@vueuse/core";
+import { backendUrl } from "@/configuration"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  async beforeMount() {
+    const { data } = await useFetch(backendUrl).json();
+    console.table(data);
   },
 });
 </script>
