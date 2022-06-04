79f1dbfb748493d8a688c2d1b32b3cc2dbadb10f
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2022-06-04 19:41:27 -0300

    Change vue configuration to allow backend call

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
+  devServer: {
+    proxy: process.env.VUE_APP_BACKEND_ENV || "http://localhost:8000",
+  },
   transpileDependencies: true,
 });
