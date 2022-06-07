9648750dd61557764d0a8460782f96f6fcdac9c0
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2022-06-06 23:03:48 -0300

    Configuring UI kit
    In this commit, some changes have been made and they might be better
    to have been done in another commit. Atomic commits are always better

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import { createVuestic, GlobalConfig } from "vuestic-ui";
+import "vuestic-ui/dist/vuestic-ui.css";
 
-createApp(App).use(router).mount("#app");
+const config: GlobalConfig = {
+  colors: {
+    brown_100: "#F2CD6D",
+    brown_200: "#D9B14C",
+    brown_300: "#BF9730",
+    brown_400: "#A67A08",
+    brown_500: "#8C6500",
+  },
+};
+
+createApp(App).use(router).use(createVuestic({ config })).mount("#app");
